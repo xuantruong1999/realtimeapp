3aa7cda19c3d1b5ab78a4ad113bca3c43c7db3bc
Truong Nguyen <kadinxuantruong1999@gmail.com>
2022-08-14 00:26:37 +0700

    add html css account/signup

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,8 +1,26 @@
+const User = require('../models/user.model');
 
 const login = (req, res) => {
-    res.render('account/login.pug', {title: "Login Page"});
+    res.render('account/login.pug', { title: "Login Page" });
+}
+
+const signup = (req, res) => {
+    res.render('account/signup.pug', { title: "Sign Up!" })
+}
+
+const create = (req, res) => {
+    try {
+        const newUser = new User(req.body)
+        newUser.Save(err => {
+            throw err;
+        })
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
-    login
+    login,
+    signup,
+    create
 }
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const accountController = require("../controllers/account.controller");
 
 router.get("/login", accountController.login);
+router.get("/signup", accountController.signup);
+router.post("/create", accountController.create)
 module.exports = router;
